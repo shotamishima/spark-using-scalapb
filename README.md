d27bb770c8f25f16bc19677162890fb5570a95d6
shotamishima <sm4mma@gmail.com>
2022-03-12 23:29:57 +0900

    Add application code

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ## Reference
 [https://scalapb.github.io/docs/sparksql/](https://scalapb.github.io/docs/sparksql/)
 
+## Usage
+
+1. Build
+
+```shell
+$ sbt assembly
+
+```
+
+2. Submit the job
+
+```shell
+$ $SPARK_HOME/bin/spark-submit \
+--jars . \
+--class myexample.RunDemo \
+target/scala-2.12/spark-using-scalapb-assembly-0.1.0-SNAPSHOT.jar
+
+```
---
+++ src/main/scala/RunDemo.scala
@@ -0,0 +1,62 @@
+package myexample
+
+import com.example.protos.demo._
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.rdd.RDD
+import scalapb.spark.Implicits._
+import scalapb.spark.ProtoSQL
+
+object RunDemo {
+
+  def main(Args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("ScalaPB Demo").master("local[2]").getOrCreate()
+
+    val sc = spark.sparkContext
+
+    val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, testData)
+
+    val personsDS1: Dataset[Person] = personsDF.as[Person]
+
+    val personsDS2: Dataset[Person] = personsDF.as[Person]
+
+    personsDS1.show()
+    personsDS2.show()
+    
+    personsDF.createOrReplaceTempView("persons")
+    spark.sql("SELECT name, age, gender, size(addresses) FROM persons").show()
+
+    spark.sql("SELECT name, age, gender, size(addresses) FROM persons WHERE age > 30")
+      .collect
+      .foreach(println)
+  }
+
+  val testData: Seq[Person] = Seq(
+    Person().update(
+      _.name := "Joe",
+      _.age := 32,
+      _.gender := Gender.MALE),
+    Person().update(
+      _.name := "Mark",
+      _.age := 21,
+      _.gender := Gender.MALE,
+      _.addresses := Seq(
+        Address(city = Some("San Francisco"), street = Some("3rd Street"))
+      )),
+    Person().update(
+      _.name := "Steven",
+      _.gender := Gender.MALE,
+      _.addresses := Seq(
+        Address(city = Some("San Francisco"), street = Some("5th Street")),
+        Address(city = Some("Sunnyvale"), street = Some("Wolfe"))
+      )),
+    Person().update(
+      _.name := "Batya",
+      _.age := 11,
+      _.gender := Gender.FEMALE))
+}
