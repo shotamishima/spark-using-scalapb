b90105f8fa39c77e281dc78d920c75530377ee53
shotamishima <sm4mma@gmail.com>
2022-03-25 23:49:02 +0900

    WIP

--- src/main/scala/RunDemo.scala
+++ src/main/scala/RunDemo.scala
@@ -25,72 +25,145 @@ object RunDemo {
     // INFOのログ出力を止める
     Logger.getLogger("org").setLevel(Level.OFF)
   
-    val spark = SparkSession.builder()
-      .appName("ScalaPB Demo")
-      .master("local[2]")
-      .getOrCreate()
+    // val spark = SparkSession.builder()
+    //   .appName("ScalaPB Demo")
+    //   .master("local[2]")
+    //   .getOrCreate()
 
-    val sc = spark.sparkContext
+    // val sc = spark.sparkContext
 
-    // protocol buffersで生成されたcase classに基づいてデータフレームを生成する
-    val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, testData)
+    // // protocol buffersで生成されたcase classに基づいてデータフレームを生成する
+    // val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, testData)
 
-    // data frame
-    println("data frame of person message --------------------------")
-    personsDF.printSchema()
-    personsDF.show()
+    // // data frame
+    // println("data frame of person message --------------------------")
+    // personsDF.printSchema()
+    // personsDF.show()
     
-    personsDF.createOrReplaceTempView("persons")
-    spark.sql("SELECT * FROM persons WHERE age > 32").show()
-    // data set 
-    println("data set of person message --------------------------")
-    val personsDS1: Dataset[Person] = personsDF.as[Person]
-    // personsDS1.collect().foreach(println)
+    // personsDF.createOrReplaceTempView("persons")
+    // spark.sql("SELECT * FROM persons WHERE age > 32").show()
+    // // data set 
+    // println("data set of person message --------------------------")
+    // val personsDS1: Dataset[Person] = personsDF.as[Person]
+    // // personsDS1.collect().foreach(println)
 
-    personsDS1.show()
+    // personsDS1.show()
     
-    // people message
-    val peopleDF: DataFrame = ProtoSQL.createDataFrame(spark, Vector(PersonList(testData)))
-    println("data frame of people message --------------------------")
-    peopleDF.printSchema()
-    peopleDF.show()
+    // // people message
+    // val peopleDF: DataFrame = ProtoSQL.createDataFrame(spark, Vector(PersonList(testData)))
+    // println("data frame of people message --------------------------")
+    // peopleDF.printSchema()
+    // peopleDF.show()
     
-    // from binary to protos and back
-    // Seq(Person)の１要素ずつmapでバイナリに変換していく。toByteArrayはPersonのメソッド
-    println("from binary to protos and back --------------------------")
-    val binaryDS: Dataset[Array[Byte]] = spark.createDataset(testData.map(_.toByteArray))
-    binaryDS.show()
-    // binaryDSの行ごとにrow -> parseFrom(row)
-    val protosDS: Dataset[Person] = binaryDS.map(Person.parseFrom(_))
-    protosDS.show()
+    // // from binary to protos and back
+    // // Seq(Person)の１要素ずつmapでバイナリに変換していく。toByteArrayはPersonのメソッド
+    // println("from binary to protos and back --------------------------")
+    // val binaryDS: Dataset[Array[Byte]] = spark.createDataset(testData.map(_.toByteArray))
+    // binaryDS.show()
+    // // binaryDSの行ごとにrow -> parseFrom(row)
+    // val protosDS: Dataset[Person] = binaryDS.map(Person.parseFrom(_))
+    // protosDS.show()
 
-    def encode(person: Person): Unit = {
-      val file = new FileOutputStream("data/tmp.pb")
-      try {
-        person.writeTo(file)
-      } finally {
-        file.close()
-      }
+    // protosDS.foreach { person => 
+    //   val file = new FileOutputStream("data/tmp.pb")
+    //   try {
+    //     person.writeTo(file)
+    //   } finally {
+    //     file.close()
+    //   }
+    // }
+
+    // // udf
+    // println("UDFs ---------------------------------------------------")
+    // val parsePersons = ProtoSQL.udf { bytes: Array[Byte] => Person.parseFrom(bytes)}
+    // // stringtocolumnの使用でエラー
+    // // binaryDS.select(col("person")).withColumn("person", parsePersons(col("value"))).show()
+    
+  // }
+
+  // val testData = for(i <- 1 to 5) yield {
+    // Person(
+    //   name = Some("Joe"),
+    //   age = Some(30 + i),
+    //   gender = if (i%2 == 0) Some(Gender.MALE) else Some(Gender.FEMALE),
+    //   addresses = Seq(
+    //     Address(city = Some("San Francisco"), street = Some(s"No.$i"))
+    //   )
+    // )
+  // }
+  
+    def initSpark(): SparkSession = {
+      SparkSession.builder()
+        .appName("ScalaPB Demo")
+        .master("local[2]")
+        .getOrCreate()
     }
     
-    protosDS.map(encode(_))
+    def addPerson(path: String): Unit = {
+      
+      val spark = initSpark()
 
-    // udf
-    println("UDFs ---------------------------------------------------")
-    val parsePersons = ProtoSQL.udf { bytes: Array[Byte] => Person.parseFrom(bytes)}
-    // stringtocolumnの使用でエラー
-    // binaryDS.select(col("person")).withColumn("person", parsePersons(col("value"))).show()
-    
-  }
+      // declare message contents
+      val newPerson = for (i <- 1 to 5) yield {
+        Person(
+          name = Some("Joe"),
+          age = Some(30 + i),
+          gender = if (i%2==0) Some(Gender.MALE) else Some(Gender.FEMALE),
+          addresses = Vector(
+            Address(city = Some("San Francisco"), street = Some(s"No.$i"))
+          )
+        )
+      }
 
-  val testData = for(i <- 1 to 5) yield {
-    Person(
-      name = Some("Joe"),
-      age = Some(30 + i),
-      gender = if (i%2 == 0) Some(Gender.MALE) else Some(Gender.FEMALE),
-      addresses = Seq(
-        Address(city = Some("San Francisco"), street = Some(s"No.$i"))
-      )
-    )
+      val personsDF: DataFrame = ProtoSQL.createDataFrame(spark, newPerson)
+      val personsDS: Dataset[Person] = personsDF.as[Person]
+      
+      personsDS.foreach { person =>
+        val file = new FileOutputStream(path)
+        println(person.age.getOrElse(-1))
+        try {
+          person.writeTo(file)
+        } finally {
+          file.close()
+        }
+      }
+    }
+
+    def addPeople(path: String): Unit = {
+      
+      val spark = initSpark()
+      
+      // declare message contents
+      val newPerson = for (i <- 1 to 5) yield {
+        Person(
+          name = Some("Joe"),
+          age = Some(30 + i),
+          gender = if (i%2==0) Some(Gender.MALE) else Some(Gender.FEMALE),
+          addresses = Vector(
+            Address(city = Some("San Francisco"), street = Some(s"No.$i"))
+          )
+        )
+      }
+      
+      // create dataframe from vector
+      val peopleDF: DataFrame = ProtoSQL.createDataFrame(spark, Vector(PersonList(newPerson)))
+      val peopleDS: Dataset[PersonList] = peopleDF.as[PersonList]
+      
+      peopleDS.printSchema()
+      peopleDF.show()
+      // encode and output to stream
+      // peopleDS.foreach { people =>
+      //   val file = new FileOutputStream(path)
+      //   try {
+      //     people.writeTo(path)
+      //   } finally {
+      //     file.close()
+      //   }
+      // }
+    }
+
+    // addPeople("data/tmp2.pb")
+    addPerson("data/tmp_person.pb")
+  
   }
 }
